5a99803ee8a70196221e6032207f445819ca8b8e
wwidi <wwidi4087@gmail.com>
2025-10-17 14:04:25 +0700

    no13 regex.php

--- regex.php
+++ regex.php
@@ -24,3 +24,13 @@ $text = 'I like apple pie.';
 
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output: "I like banana pie."
+
+echo "<br>";
+$pattern = '/go*d/'; // Cocokkan "gd", "god", "good", "goooood", dll.
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
