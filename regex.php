d9b0f58d96d5c0c6170d1c0f5e237d4fc917836b
wwidi <wwidi4087@gmail.com>
2025-10-17 14:02:12 +0700

    no9 regex.php

--- regex.php
+++ regex.php
@@ -17,3 +17,10 @@ if (preg_match($pattern, $text, $matches)) {
 } else {
     echo "Tidak ada yang cocok!";
 }
+echo "<br>";
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: "I like banana pie."
